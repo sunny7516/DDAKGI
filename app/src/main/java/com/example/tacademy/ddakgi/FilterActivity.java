99a794359d01e7d82832b2d5be70bcfe88207330
sunny7516 <limbo001@naver.com>
2017-02-03 22:44:53 +0900

    filte화면에 액션바 마무리하고, WriteTab에 글 유형 선택 버튼 추가

--- app/src/main/java/com/example/tacademy/ddakgi/FilterActivity.java
+++ app/src/main/java/com/example/tacademy/ddakgi/FilterActivity.java
@@ -1,10 +1,38 @@
 package com.example.tacademy.ddakgi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class FilterActivity extends AppCompatActivity {
+
+    // Filter Button을 Toolbar에 적용
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.filter_layout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId){
+            case R.id.filter_cancel:
+                this.finish();
+                /*
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);*/
+                break;
+            case R.id.filter_reset:
+                break;
+            case R.id.filter_apply:
+                break;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
